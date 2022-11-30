c1b3e604d939e712d57e45924c8f70655c22f974
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-11-30 23:16:35 +0800

    Login page final

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include("time&date.php");
 $is_invalid = false;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -36,27 +37,35 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <title>Login</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="login-form-container">
-        <img src="img/ggd-logo.png" alt="" class="image">
+    <nav>
+        <h4><?php echo "".$date; ?></h4>
+        <h4><?php echo "".$time; ?></h4>
+    </nav>
 
-        <?php if ($is_invalid): ?>
-            <em>Wrong email or password!</em>
-        <?php endif; ?>
-        
-        <hr>
-
-        <form method="post">
-                <input placeholder="Username" type="text" name="email" id="email"
-                value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+        <div class="login-form-container">
+            <img src="img/ggd-logo.png" alt="" class="image">
             
-                <input placeholder="Password" type="password" name="password" id="password">
+            <hr>
 
-                <button>Login</button>
-        </form>
+            <?php if ($is_invalid): ?>
+                <em>Wrong email or password!</em>
+            <?php endif; ?>
 
-        <a href="#">forgot password?</a>
-    </div>
+            <form method="post">
+                    <input placeholder="Username" type="text" name="email" id="email"
+                    value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                
+                    <input placeholder="Password" type="password" name="password" id="password">
+
+                    <button>Login</button>
+            </form>
+
+            <a href="#">forgot password?</a>
+        </div>
+
 </body>
 </html>
