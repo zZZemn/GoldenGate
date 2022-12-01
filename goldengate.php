81281e8fd48e94407683091503bb4b330540b75c
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-01 17:03:55 +0800

    Top nav final style

--- goldengate.php
+++ goldengate.php
@@ -1,5 +1,5 @@
 <?php 
-
+include("time&date.php");
 session_start();
 
 if (isset($_SESSION["user_id"])) {
@@ -17,65 +17,23 @@ if (isset($_SESSION["user_id"])) {
 <html>
 <head>
     <title>Golden Gate Drugstore</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/goldengate.css">
 </head>
 <body>
     <?php if (isset($user)): ?>
-        <nav>
-            <img src="img/logo.png" alt="Nails Haven">
-            <ul>
-                <li><a href="admin-profile.php" class="name"><?= htmlspecialchars($user['name']) ?></a></li>
-                <li><a class="Current-Reservation" href="admin.php" class="Current-Reservation">Current Reservation</a></li>
-                <li><a href="cancelled.php">Cancelled Reservation</a></li>
-                <li><a href="#">Inventory</a></li>
-                <li><a href="#">Sales</a></li>
-                <li><a href="logout.php">Log Out</a></li>
-            </ul>
+        <nav class="top-nav">
+            <img src="img/ggd-logo.png" alt="GGD">
+            <h1>GOLDEN GATE DRUGSTORE</h1>
+            <h5><?php echo $time ?></h5>
         </nav>
-        <div class="reservation">
-            <h1 class="app-title">Appointment</h1>
-            <table>
-                <tr class="table-head">
-                    <td>ID</td>
-                    <td>Name</td>
-                    <td>Contact</td>
-                    <td>Date</td>
-                    <td>Time</td>
-                    <td>Reference Number</td>
-                    <td colspan="2">Action</td>
-                </tr>
-
-                <?php  if ($reservation_result->num_rows > 0) {
-                            while($row = $reservation_result->fetch_assoc()) {
-                                $time = $row['app_time'];
-                                $mod_time = substr($time,0,5);
-                                    echo "<tr>
-                                            <td class="."userID".">".$row['id']."</td>
-                                            <td>".$row['f_name']." ".$row['l_name']."</td>
-                                            <td>".$row['contact_no']."</td>
-                                            <td>".$row['app_date']."</td>
-                                            <td>".$mod_time."</td>
-                                            <td>".$row['ref_num']."</td>
-                                            <td class="."btn"." style="."border-right:none".">
-                                                <a href='view-row.php?id=".$row['id']."'>View</a> 
-                                            </td>
-                                            <td class="."btn2"." style="."border-left:none".">
-                                                <a href='delete-row.php?id=".$row['id']."'>Delete</a> 
-                                            </td>
-                                            </tr>";
-                            }
-                        }           
-                ?>
-                
-            </table>
-        </div>
 
 
     <?php else: ?>
-        <div class="noacc">
+        <div class="no-account-selected">
             <h1>No account selected</h1>
-            <p class="ls"><a href="index.php">Log in</a> or <a href="signup.html">sign up</a></p>
+            <p class="Login"><a href="index.php">Login</a>
         </div>
     <?php endif; ?>
-    
 </body>
 </html>
