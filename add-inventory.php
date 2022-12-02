a2a83e3581cfeedc107aea412abc10610dafe688
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-02 15:50:37 +0800

    adding products to inventory

--- add-inventory.php
+++ add-inventory.php
@@ -10,8 +10,30 @@ if (isset($_SESSION["user_id"])) {
     $result = $connect->query($sql);
     $user = $result->fetch_assoc();
 
-    $inventory = "SELECT * FROM inventory ORDER by ex_date";
-    $inventory_result = $connect->query($inventory);
+    
+    
+    if(isset($_POST['add']))
+    {
+        
+        $pro_code = $_POST['product_code'];
+        $del_price = $_POST['delivery_price'];
+        $quantity = $_POST['delivery_quantity'];
+        $del_date = $_POST['delivery_date'];
+        $ex_date = $_POST['expiration_date'];
+
+        $add_query = "INSERT INTO inventory(`del_price`, `del_qty`, `ex_date`, `del_date`, `pro_code`) VALUES ('$del_price','$quantity','$ex_date','$del_date','$pro_code')";
+        $adding_products_qty = "UPDATE products SET `quantity` = quantity+$quantity WHERE pro_code = $pro_code";
+    
+            if($connect->query($add_query) == true && $connect->query($adding_products_qty) == true)
+                {
+                    header("location: inventory.php");
+                }
+        
+            else
+                {
+                    echo '<script type="text/javascript">alert("Invalid Input!");</script>';     
+                }
+    }
 }
 
 ?>
@@ -19,9 +41,9 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Inventory</title>
+    <title>Inventory Maintenance</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/inventory.css">
+    <link rel="stylesheet" href="css/add-inventory.css">
 </head>
 <body>
     <?php if (isset($user)): ?>
@@ -83,52 +105,55 @@ if (isset($_SESSION["user_id"])) {
             </table>
         </nav>
 
-        <div class="inventory-table-container">
-            <table class="inventory-table">
-                <tr class="inventory-table-top">
-                    <td colspan="2">Action</td>
-                    <td>ID</td>
-                    <td>Product Code</td>
-                    <td>Product Name</td>
-                    <td>Delivery Price</td>
-                    <td>Selling Price</td>
-                    <td>Quantity</td>
-                    <td>Delivery Date</td>
-                    <td>Expiration Date</td>
+        <h3 class="add-inventory-title">Add Product</h3>
+        <div class="add-inventory-container">
+        <form action="" method="post">
+            <table class="add-inventory-table">
+                <tr class="product-code-tr">
+                    <td>Product Code:</td>
+                    <td class="right">
+                        <input type="text" placeholder="Enter Product Code" name="product_code" list="product_codes">
+                        
+                            <?php 
+                                $product = "SELECT * FROM products";
+                                $product_result = $connect->query($product);
+
+                                if($product_result->num_rows > 0)
+                                {
+                                    echo "<datalist id='product_codes'>";
+
+                                    while($row = $product_result->fetch_assoc())
+                                    {
+                                        echo "<option value=".$row['pro_code'].">";
+                                    }
+                                }
+                            ?>
+                    </td>
                 </tr>
-
-                <?php 
-                    if($inventory_result->num_rows > 0)
-                    {
-                        while($row = $inventory_result->fetch_assoc())
-                        {
-                            $product = "SELECT * FROM products
-                            WHERE pro_code = $row[pro_code]";
-                            $product_result = $connect->query($product);
-                            $product_table = $product_result->fetch_assoc();
-                            
-                            echo "<tr>
-                                        <td class='action-buttons'><a href='#'><img src='img/trash-outline.svg' alt=''></a></td>
-                                        <td class='action-buttons'><a href='#'><img src='img/create-outline.svg' alt=''></a></td>
-                                        <td>".$row['invent_no']."</td>
-                                        <td>".$row['pro_code']."</td>
-                                        <td>".$product_table['pro_name']."</td>
-                                        <td>".$row['del_price']."</td>
-                                        <td>".$product_table['price']."</td>
-                                        <td>".$row['del_qty']."</td>
-                                        <td>".$row['del_date']."</td>
-                                        <td>".$row['ex_date']."</td>
-                                    </tr>";
-                        }
-                    }
-                ?>
+                <tr>
+                    <td>Delivery Price:</td>
+                    <td class="right"><input type="text" placeholder="Delivery Price" name="delivery_price"></td>
+                </tr>
+                <tr>
+                    <td>Quantity:</td>
+                    <td class="right"><input type="number" placeholder="Qty" name="delivery_quantity"></td>
+                </tr>
+                <tr>
+                    <td>Delivery Date:</td>
+                    <td class="right"><input type="date" name="delivery_date"></td>
+                </tr>
+                <tr>
+                    <td>Expiration Date:</td>
+                    <td class="right"><input type="date" name="expiration_date"></td>
+                </tr>
             </table>
+            <input type="submit" class="add-inventory-add-button" name="add" value="Add">
+            <a class="cancel-inventory-button" href="inventory.php">Cancel</a>
+        </form>
         </div>
 
-        <div class="table-inventory-buttons">
-            <a href="add-inventory.php">Add Product</a>
-            <a href="#">Report</a> 
-        </div>
+        
+
     
         
 
