09c69c40c0ead8305b77b568150aa2b05d8c7a15
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-04 17:32:32 +0800

    computation

--- pos.php
+++ pos.php
@@ -11,17 +11,36 @@ if (isset($_SESSION["user_id"])) {
     $result = $connect->query($sql);
     $user = $result->fetch_assoc();
     }
+    //---------------------
 
     $vat_disc_Query = "SELECT * FROM payment_vat_discount";
     $vat_disc_result = $connect->query($vat_disc_Query);
     $vat_disc = $vat_disc_result->fetch_assoc();
 
-    $vat = $vat_disc['vat'];
+    $tax = $vat_disc['vat'];
     $discount = $vat_disc['discount'];
 
     $cust_disc = 0;
     $subtotal = 0;
+    $total = 0;
 
+    $tblPOSquery = "SELECT * FROM current_pos_operation";
+        $tblPOSres = $connect->query($tblPOSquery);
+        if($tblPOSres->num_rows > 0)
+        {
+            while($row_pos = $tblPOSres->fetch_assoc())
+            {
+                $row_amount = $row_pos['amount'];
+                $subtotal += $row_amount;
+            }
+        }
+        
+            $vat = $subtotal * $tax;
+            $total = $subtotal + $vat;
+            $finalTot = $total - $cust_disc;
+        
+
+//------------------------------------
     if (isset($_POST['add'])) {
         $pro_code = $_POST['pro_code'];
         $quantity = $_POST['qty'];
@@ -41,21 +60,14 @@ if (isset($_SESSION["user_id"])) {
 
         $connect->query($tblPOS);
 
-        
+        //subtotal computation
+        $subtotal += $amount;
+        $vat = $subtotal * $tax;
+        $total = $subtotal + $vat;
+        $finalTot = $total - $cust_disc;
 }
         
 
-        //subtotal computation
-        $tblPOSquery = "SELECT * FROM current_pos_operation";
-        $tblPOSres = $connect->query($tblPOSquery);
-        if($tblPOSres->num_rows > 0)
-        {
-            while($row_pos = $tblPOSres->fetch_assoc())
-            {
-                $row_amount = $row_pos['amount'];
-                $subtotal += $row_amount;
-            }
-        }
 
 ?>
 
@@ -160,26 +172,27 @@ if (isset($_SESSION["user_id"])) {
                                 <th>VAT</th>
                                 <th>TOTAL</th>
                                 <th>CHANGE</th>
+                                
                             </tr>
                             <tr>
                                 <td><input type="text" name="vat" placeholder="VAT" readonly value="<?php echo $vat; ?>"></td>
-                                <td><input type="text" name="total" placeholder="Total" readonly></td>
+                                <td><input type="text" name="total" placeholder="Total" readonly value="<?php echo $finalTot ?>"></td>
                                 <td class="change-td"><input type="text" name="change" placeholder="Change" readonly></td>
-                                <td><button class="btn2 print">Print</button></td>
                             </tr>
                             
                             <tr>
                                 <th>DISCOUNT</th>
                                 <th>SUBTOTAL</th>
                                 <th>PAYMENT</th>
-                                <td><input class="btn2 settle" type="submit" name="settle" value="Settle"></td>
-                                
+                                <td class="right"><input class="btn2  compute" type="submit" name="compute" value="Compute"></td>
+                                <td class="left"><button class="btn2 print">Print</button></td>
                             </tr>
                             <tr>
                                 <td><input type="text" name="discount" placeholder="Discount" readonly value="<?php echo $cust_disc ?>"></td>
                                 <td><input type="text" name="subtotal" placeholder="Subtotal" readonly value="<?php echo $subtotal ?>"></td>
                                 <td class="payment-td"><input type="number" name="payment" placeholder="Payment"></td>
-                                <td><a href="#" class="cancel">Cancel</a></td>
+                                <td class="right"><input class="btn2 settle" type="submit" name="settle" value="Settle"></td>
+                                <td class="left"><a href="#" class="cancel">Cancel</a></td>
                             </tr>
                         </table>
                     </form>
