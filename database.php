8471772725bea71358ceb7c5a94aa9aefff14ed5
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-07 18:53:52 +0800

    init

--- database.php
+++ database.php
@@ -7,10 +7,4 @@ $dbName = "golden_gate";
 
 $connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 
-/*
-// Check connection
-if ($mysqli -> connect_error) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}*/
 ?>
